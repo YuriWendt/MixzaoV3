a3f59e22381e3c3f93c3226d64b26984ffca11c5
= <=>
2023-09-29 22:45:20 -0400

    feat(firebase): hosting the app in firebase

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -5,7 +5,7 @@ axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 const API_USER = axios.create({
-    baseURL: process.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API_USER;
